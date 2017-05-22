f249d9a7aa6378934a759aedc50c17a6ea70c0b1
leehwangchun <elluard@me.com>
2017-05-22 17:41:31 +0900

    # 테스트 코드 수정

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,16 @@
+import java.sql.Date
+import javax.inject.Inject
+
+import models.{Account, AccountDataAccess}
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
+import org.specs2.mock.Mockito
 import play.api.test._
 import play.api.test.Helpers._
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 /**
  * Add your spec here.
@@ -11,12 +18,11 @@ import play.api.test.Helpers._
  * For more information, consult the wiki.
  */
 @RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec @Inject()(implicit e : ExecutionContext) extends Specification with Mockito{
 
   "Application" should {
-
     "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
+      route(FakeRequest(GET, "/bojjum")) must beNone
     }
 
     "render the index page" in new WithApplication{
@@ -24,7 +30,7 @@ class ApplicationSpec extends Specification {
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      contentAsString(home) must contain ("Thomas.com")
     }
   }
 }
